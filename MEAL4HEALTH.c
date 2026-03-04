9a2555eea3bbba1e74292f259fa82e03f0245e07
lanaquibuloy-code <lana_quibuloy@dlsu.edu.ph>
2026-03-04 16:48:23 +0800

    Update MEAL4HEALTH.c
    added the display menu for both update and access recipe box and made the password requirements as a struct

--- MEAL4HEALTH.c
+++ MEAL4HEALTH.c
@@ -51,60 +51,105 @@ typedef struct {
     int Calories;
 }nFoodInfo;
 
+typedef struct {
+	str20 user;
+	str20 pass;
+} nAdminLogin;
+
 /*
 ===============================
 |         FUNCTIONS           |
 ===============================
 */
 void displayMainMenu(char *nChoice, int *ProgramRuns)
-{
-    
-    while (*ProgramRuns)
-    {
-        printf("+===================================+\n");
-        printf("||%-33s||\n", "     Meals for Health");
-        printf("+===================================+\n");
-        printf("||%-33s||\n", " What would you like to do?");
-        printf("+===================================+\n");
-        printf("||%-33s||\n", " U. Update Recipe Box");
-        printf("||%-33s||\n", " A. Access Recipe Box");
-        printf("||%-33s||\n", " E. Exit Menu");
-        printf("+===================================+\n");
-        printf("> ");
-        
-        scanf(" %c", nChoice);
-        
-        *ProgramRuns = 0;
-        
-        return;
-        
-    }
-    
+{	
+		printf("+===================================+\n");
+		printf("||%-33s||\n", "     Meals for Health");
+		printf("+===================================+\n");
+		printf("||%-33s||\n", " What would you like to do?");
+		printf("+===================================+\n");
+		printf("||%-33s||\n", " U. Update Recipe Box");
+		printf("||%-33s||\n", " A. Access Recipe Box");
+		printf("||%-33s||\n", " E. Exit Menu");
+		printf("+===================================+\n");
+		printf("> "); 
+		
+		scanf(" %c", nChoice);
+	
 }
 
-void chooseUpdate(char *user, char *pass, char *nChoice, int *ProgramRuns)
+void chooseUpdate(nAdminLogin *login, int *nSuccess)
 {
     
     printf("\nEnter username: ");
-    scanf("%s", user);
+    scanf("%20s", login->user);
     
     printf("Enter password: ");
-    scanf("%s", pass);
+    scanf("%20s", login->pass);
 
-    if (strcmp(user, "admin") == 0 && strcmp(pass, "ad1234") == 0)
-         printf("\nEntering update mode.. ");
+    if (strcmp(login->user, "admin") == 0 && strcmp(login->pass, "ad1234") == 0)
+    {
+    	printf("\nEntering update mode.. \n\n");
+    	*nSuccess = 1;
+	}
   
     else
     {
         printf("\nInvalid username or password");
         printf("\nReturning to main menu..\n\n");
-        displayMainMenu(nChoice, ProgramRuns);
-        *ProgramRuns = 1;
+        *nSuccess = 0;
     }
-
-
 }
 
+void displayUpdateRecipeBox(char *nChoice)
+{
+	printf("+===================================+\n");
+	printf("||%-33s||\n", "     UPDATE RECIPE BOX");
+	printf("+===================================+\n");
+	printf("||%-33s||\n", "       CALORIE DATABASE");
+	printf("+===================================+\n");
+	printf("||%-33s||\n", " 1. Add Food-Calorie Info");
+	printf("||%-33s||\n", " 2. View Food-Calorie Chart");
+	printf("||%-33s||\n", " 3. Save Calorie Info");
+	printf("||%-33s||\n", " 4. Load Calorie Info");
+	
+	printf("+===================================+\n");
+	printf("||%-33s||\n", "        RECIPE DATABASE");
+	printf("+===================================+\n");
+	printf("||%-33s||\n", " 5. Add Recipe");
+	printf("||%-33s||\n", " 6. Modify Recipe");
+	printf("||%-33s||\n", " 7. Delete Recipe");
+	printf("||%-33s||\n", " 8. List Recipe Titles");
+	printf("||%-33s||\n", " 9. Scan Recipes");
+	printf("||%-33s||\n", " S. Search Recipe by Title");
+	printf("||%-33s||\n", " E. Export Recipes");
+	printf("||%-33s||\n", " I. Import Recipes");
+	printf("+===================================+\n");
+	printf("||%-33s||\n", " R. Exit Menu");
+	printf("+===================================+\n");
+	printf("> "); 
+	
+	scanf(" %c", nChoice);
+}
+
+void displayAccessRecipeBox(char *nChoice)
+{
+	printf("+===================================+\n");
+	printf("||%-33s||\n", "     ACCESS RECIPE BOX");
+	printf("+===================================+\n");
+	printf("||%-33s||\n", " 1. Import Recipes");
+	printf("||%-33s||\n", " 2. Scan Recipes");
+	printf("||%-33s||\n", " 3. Search Recipe by Title");
+	printf("||%-33s||\n", " 4. Generate Shopping List");
+	printf("||%-33s||\n", " 5. Scan Recipes by Ingredient");
+	printf("||%-33s||\n", " 6. Recommend Menu");
+	
+	printf("||%-33s||\n", " R. Exit Menu");
+	printf("+===================================+\n");
+	
+	printf("> ");
+	scanf(" %c", nChoice);
+}
 
 /*
 ===============================
@@ -112,46 +157,156 @@ void chooseUpdate(char *user, char *pass, char *nChoice, int *ProgramRuns)
 ===============================
 */
 
-
 int main(void)
 {
-    int ProgramRuns = 1;
-    char nChoice;
-    char user[10], pass[10];
-
-    while(ProgramRuns)
-    {
-        displayMainMenu(&nChoice, &ProgramRuns);
-        
-        switch(nChoice)
-        {
-            case 'U':
-            case 'u':
-                printf("Update mode...");
-                chooseUpdate(user, pass, &nChoice, &ProgramRuns);
-                break;
-            
-            case 'A':
-            case 'a':
-                printf("Accessing mode...\n");
-                ProgramRuns = 0;
-                break;
-            
-            case 'E':
-            case 'e':
-                printf("Exiting...\n");
-                ProgramRuns = 0;
-                break;
-                
-            default:
-                printf("Invalid Option. Try again.\n\n");
-                ProgramRuns = 1; // goes back to menu
-                break;
-        
-        }
-        
-    }
-    
-    return 0;
-    
+	int ProgramRuns = 1;
+	char nChoice;
+	
+	while(ProgramRuns) 
+	{
+		displayMainMenu(&nChoice, &ProgramRuns);
+		
+		switch(nChoice)
+		{	
+		//CHOOSING UPDATE RECIPE 
+			case 'U':
+			case 'u':
+				{
+					nAdminLogin login;
+					int loginSuccess;
+					
+					chooseUpdate(&login, &loginSuccess);
+					
+					if(loginSuccess) //if login is succesful
+					{
+						char NewChoice;
+						int InUpdateChoice = 1;
+						
+						while(InUpdateChoice)
+						{
+							displayUpdateRecipeBox(&NewChoice);
+							
+							switch(NewChoice)
+							{
+								case '1':
+									printf("WALA PA");
+									break;
+								
+								case '2':
+									printf("WALA PA");
+									break;
+								case '3':
+									printf("WALA PA");
+									break;
+								case '4':
+									printf("WALA PA");
+									break;
+								case '5':
+									printf("WALA PA");
+									break;
+								case '6':
+									printf("WALA PA");
+									break;
+								case '7':
+									printf("WALA PA");
+									break;
+								case '8':
+									printf("WALA PA");
+									break;
+								case '9':
+									printf("WALA PA");
+									break;
+								case 'S':
+								case 's':
+									printf("WALA PA");
+									break;
+								case 'E':
+								case 'e':
+									printf("WALA PA");
+									break;
+								case 'I':
+								case 'i':
+									printf("WALA PA");
+									break;
+									
+									//RETURN TO MENU
+								case 'R':
+								case 'r':
+									InUpdateChoice = 0; 
+									break;
+									
+								default:
+									printf("Invalid Username or password\n");
+									break;
+							}
+						}
+					}
+					else {
+						printf("Invalid Option.\n");
+					}
+				}
+				break;
+				
+			//CHOOSING ACCESS RECIPE
+			case 'A': 
+			case 'a':
+				{
+					char NewChoice;
+					int InAccessChoice = 1;
+					
+					while(InAccessChoice)
+					{
+						displayAccessRecipeBox(&NewChoice);
+						
+						switch(NewChoice)
+						{
+							case '1':
+								printf("WALA PA\n");
+								break;
+							case '2':
+								printf("WALA PA\n");
+								break;
+							case '3':
+								printf("WALA PA\n");
+								break;
+							case '4':
+								printf("WALA PA\n");
+								break;
+							case '5':
+								printf("WALA PA\n");
+								break;
+							case '6':
+								printf("WALA PA\n");
+								break;
+								
+							case 'R':
+							case 'r':
+								InAccessChoice = 0;
+								break;
+							
+							default:
+								printf("INVALID");
+								break;
+						}
+					}
+				}
+				break;
+			
+			case 'E':
+			case 'e':
+				printf("Exiting...\n");
+				ProgramRuns = 0;
+				break; 
+				
+			default:
+				printf("Invalid Option. Try again.\n\n");
+				ProgramRuns = 1; // goes back to menu
+				break;
+		
+		}
+		
+	}
+	
+	return 0;
+	
 }
