41f4f7223b03252505e8cecc7c7b48982fe7eac6
keionnaj <keionna_jaramillo@dlsu.edu.ph>
2026-03-04 16:05:53 +0800

    Update MEAL4HEALTH.c

--- MEAL4HEALTH.c
+++ MEAL4HEALTH.c
@@ -13,7 +13,7 @@
 #define MAX_STEPS                15
 
 //CHARACTER LIMITATIONS
-typedef char str20[21]; //NAME OF DISH & NAME OF ITEN USED 
+typedef char str20[21]; //NAME OF DISH & NAME OF ITEN USED
 typedef char str15[16]; // UNITS OF MEASUREMENT
 typedef char str70[71]; // INSTRUCSTIONS
 
@@ -23,32 +23,32 @@ typedef char str70[71]; // INSTRUCSTIONS
 ===============================
 */
 typedef struct {
-	double Qty;
-	str15 UnitofMeas;
-	str20 FoodItem;
+    double Qty;
+    str15 UnitofMeas;
+    str20 FoodItem;
 } nIngredients;
 
 typedef struct {
-	str70 directions;
+    str70 directions;
 }nInstructions;
 
 typedef struct {
-	str20 nDishName; 
-	str15 nClassification;
-	int servingSize;
-	
-	nIngredients ingredientsList[MAX_INGREDIENTS];
-	int ingreCount;
-	
-	nInstructions stepsList[MAX_STEPS];
-	int stepCount;
+    str20 nDishName;
+    str15 nClassification;
+    int servingSize;
+    
+    nIngredients ingredientsList[MAX_INGREDIENTS];
+    int ingreCount;
+    
+    nInstructions stepsList[MAX_STEPS];
+    int stepCount;
 }nRecipe;
 
 typedef struct {
-	str20 FoodItem;
-	double Qty;
-	str15 UnitofMeas;
-	int Calories;
+    str20 FoodItem;
+    double Qty;
+    str15 UnitofMeas;
+    int Calories;
 }nFoodInfo;
 
 /*
@@ -58,29 +58,53 @@ typedef struct {
 */
 void displayMainMenu(char *nChoice, int *ProgramRuns)
 {
-	
-	while (*ProgramRuns)
-	{
-		printf("+===================================+\n");
-		printf("||%-33s||\n", "     Meals for Health");
-		printf("+===================================+\n");
-		printf("||%-33s||\n", " What would you like to do?");
-		printf("+===================================+\n");
-		printf("||%-33s||\n", " U. Update Recipe Box");
-		printf("||%-33s||\n", " A. Access Recipe Box");
-		printf("||%-33s||\n", " E. Exit Menu");
-		printf("+===================================+\n");
-		printf("> "); 
-		
-		scanf("%c", nChoice);
-		
-		*ProgramRuns = 0;
-		
-		return;
-		
-	}
-	
+    
+    while (*ProgramRuns)
+    {
+        printf("+===================================+\n");
+        printf("||%-33s||\n", "     Meals for Health");
+        printf("+===================================+\n");
+        printf("||%-33s||\n", " What would you like to do?");
+        printf("+===================================+\n");
+        printf("||%-33s||\n", " U. Update Recipe Box");
+        printf("||%-33s||\n", " A. Access Recipe Box");
+        printf("||%-33s||\n", " E. Exit Menu");
+        printf("+===================================+\n");
+        printf("> ");
+        
+        scanf(" %c", nChoice);
+        
+        *ProgramRuns = 0;
+        
+        return;
+        
+    }
+    
 }
+
+void chooseUpdate(char *user, char *pass, char *nChoice, int *ProgramRuns)
+{
+    
+    printf("\nEnter username: ");
+    scanf("%s", user);
+    
+    printf("Enter password: ");
+    scanf("%s", pass);
+
+    if (strcmp(user, "admin") == 0 && strcmp(pass, "ad1234") == 0)
+         printf("\nEntering update mode.. ");
+  
+    else
+    {
+        printf("\nInvalid username or password");
+        printf("\nReturning to main menu..\n\n");
+        displayMainMenu(nChoice, ProgramRuns);
+        *ProgramRuns = 1;
+    }
+
+
+}
+
 
 /*
 ===============================
@@ -91,42 +115,43 @@ void displayMainMenu(char *nChoice, int *ProgramRuns)
 
 int main(void)
 {
-	int ProgramRuns = 1;
-	char nChoice;
-	
-	while(ProgramRuns) 
-	{
-		displayMainMenu(&nChoice, &ProgramRuns);
-		
-		switch(nChoice)
-		{	
-			case 'U':
-			case 'u':
-				printf("Update mode...");
-				ProgramRuns = 0;
-				break;
-			
-			case 'A': 
-			case 'a':
-				printf("Accessing mode...\n");
-				ProgramRuns = 0;
-				break;
-			
-			case 'E':
-			case 'e':
-				printf("Exiting...\n");
-				ProgramRuns = 0;
-				break; 
-				
-			default:
-				printf("Invalid Option. Try again.\n\n");
-				ProgramRuns = 1; // goes back to menu
-				break;
-		
-		}
-		
-	}
-	
-	return 0;
-	
+    int ProgramRuns = 1;
+    char nChoice;
+    char user[10], pass[10];
+
+    while(ProgramRuns)
+    {
+        displayMainMenu(&nChoice, &ProgramRuns);
+        
+        switch(nChoice)
+        {
+            case 'U':
+            case 'u':
+                printf("Update mode...");
+                chooseUpdate(user, pass, &nChoice, &ProgramRuns);
+                break;
+            
+            case 'A':
+            case 'a':
+                printf("Accessing mode...\n");
+                ProgramRuns = 0;
+                break;
+            
+            case 'E':
+            case 'e':
+                printf("Exiting...\n");
+                ProgramRuns = 0;
+                break;
+                
+            default:
+                printf("Invalid Option. Try again.\n\n");
+                ProgramRuns = 1; // goes back to menu
+                break;
+        
+        }
+        
+    }
+    
+    return 0;
+    
 }
